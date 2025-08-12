31eb03ed6077c268024172434918c97e1daa3a46
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-08-12 16:09:21 +0600

    add css meanmenu

--- Master/src/assets/js/main.js
+++ Master/src/assets/js/main.js
@@ -27,97 +27,11 @@
 
   var windowOn = $(window);
 
-  //! Dark Mode
-
-  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
-  // Change the icons inside the button based on previous settings
-  if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    themeToggleLightIcon.classList.remove("hidden");
-  } else {
-    themeToggleDarkIcon.classList.remove("hidden");
-  }
-
-  var themeToggleBtn = document.getElementById("theme-toggle");
-
-  themeToggleBtn.addEventListener("click", function () {
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
-
-    // if set via local storage previously
-    if (localStorage.getItem("color-theme")) {
-      if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      }
-
-      // if NOT set via local storage previously
-    } else {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      }
-    }
-  });
-
-  // Dark For Mobile
-  var themeToggleDarkIconMobile = document.getElementById(
-    "theme-toggle-dark-icon-mobile"
-  );
-  var themeToggleLightIconMobile = document.getElementById(
-    "theme-toggle-light-icon-mobile"
-  );
-
-  // Change the icons inside the button based on previous settings
-  if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    themeToggleLightIconMobile.classList.remove("hidden");
-  } else {
-    themeToggleDarkIconMobile.classList.remove("hidden");
-  }
-
-  var themeToggleBtn = document.getElementById("theme-toggle-mobile");
-
-  themeToggleBtn.addEventListener("click", function () {
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
-
-    // if set via local storage previously
-    if (localStorage.getItem("color-theme")) {
-      if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      }
-
-      // if NOT set via local storage previously
-    } else {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      }
-    }
+  // 02. Mobile Menu Js
+  $("#mobile-menu").meanmenu({
+    meanMenuContainer: ".mobile-menu",
+    meanScreenWidth: "991",
+    meanExpand: ['<i class="fal fa-plus"></i>'],
   });
 
   //! 04. Counter Js
@@ -138,13 +52,6 @@
   //! 11. PreLoader Js
   windowOn.on("load", function () {
     $(".InitLoader").fadeOut(1000);
-  });
-
-  //! Type js
-
-  $(".typeIntro").typer({
-    strings: ["Hire Me!", "Need Help?", "Contact Now"],
-    typeSpeed: 300,
   });
 
   //! 13. Body overlay Js
